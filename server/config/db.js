6cb3fefaebc8a2d34ee71a368724c2f57159c084
Ugyen Thinley <144566236+Ugyen5007@users.noreply.github.com>
2023-11-01 21:19:58 -0400

    Fixed Database compass

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,14 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books"
+
+ // "URI": "mongodb://localhost/books"
+  "URI": "mongodb+srv://dbadmin:nkcwpqDUdc8Tf4Qq@cluster0.vm2gmkd.mongodb.net/books"
 
 };
- 
 
-//let atlasDB = "mongodb+srv://dbadmin:lSNmLzUCtumF4K9g@cluster0.vm2gmkd.mongodb.net/products"
+//nkcwpqDUdc8Tf4Qq
+
+//let atlasDB = "mongodb+srv://dbadmin:lSNmLzUCtumF4K9g@cluster0.vm2gmkd.mongodb.net/books"
 
 
+//let atlasDB = "mongodb+srv://dbadmin:nkcwpqDUdc8Tf4Qq@cluster0.vm2gmkd.mongodb.net/products"
