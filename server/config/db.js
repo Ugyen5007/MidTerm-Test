be665b091089ec9ea3e5d06ac488bcfaa10b6f89
Ugyen Thinley <144566236+Ugyen5007@users.noreply.github.com>
2023-11-01 20:22:40 -0400

    Mid-Term Book List

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb://localhost/books"
+
+};
+ 
+
+//let atlasDB = "mongodb+srv://dbadmin:lSNmLzUCtumF4K9g@cluster0.vm2gmkd.mongodb.net/products"
+
+
---
+++ server/models/books.js
@@ -0,0 +1,18 @@
+
+
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
