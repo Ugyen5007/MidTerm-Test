39af9de9d291b6158ad3d1dc59afaf501d090d71
Ugyen Thinley <144566236+Ugyen5007@users.noreply.github.com>
2023-11-04 17:15:39 -0400

    fixed database

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ module.exports = {
   //local MongoDB deployment ->
 
  // "URI": "mongodb://localhost/books"
-  "URI":  "mongodb+srv://dbadmin:rCP4pMhkVXMEGD2Q@cluster0.vm2gmkd.mongodb.net/products"  
+  "URI":  "mongodb+srv://dbadmin:rCP4pMhkVXMEGD2Q@cluster0.vm2gmkd.mongodb.net/books"  
 
 };
 
